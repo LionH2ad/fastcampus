4047c36cf2f988e09cd190009a95163f409a4e63
SUNGMIN\sungmin <lion.h2ad@gmail.com>
2023-07-19 00:06:10 +0900

    kotlin_basic_part0_String_templet

---
+++ kotlin/app/src/main/java/com/example/kotlin/Example10.kt
@@ -0,0 +1,10 @@
+package com.example.kotlin
+
+fun main(){
+    val a = 10
+    val name = "성민"
+    val isHigh = true
+
+    //println("$a $name $isHigh")
+    println("$a ${name.length} $isHigh")
+}
