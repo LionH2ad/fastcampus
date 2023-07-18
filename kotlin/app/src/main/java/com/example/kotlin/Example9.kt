8aacc0d2a35024cb4c007a8753a7f9e3e37fa0d3
SUNGMIN\sungmin <lion.h2ad@gmail.com>
2023-07-18 23:59:18 +0900

    kotlin_basic_part0_type_check_casting

---
+++ kotlin/app/src/main/java/com/example/kotlin/Example9.kt
@@ -0,0 +1,49 @@
+package com.example.kotlin
+
+fun main(){
+    /*println(check("안녕"))
+    println(check(3))
+    println(check(true))*/
+    /*cast("안녕")
+    cast(3)*/
+    println(smartCast("안녕"))
+    println(smartCast(10))
+    println(smartCast(true))
+}
+
+fun check(a: Any): String {
+    return when (a) {
+        is String -> {
+            "문자열"
+        }
+        is Int -> {
+            "숫자"
+        }
+        else -> {
+            "몰라요"
+        }
+    }
+    /*return  if(a is String){
+        "문자열"
+    } else if(a is Int){
+        "숫자"
+    } else{
+        "몰라요"
+    }*/
+}
+
+fun cast(a: Any){
+    //val result =  a as? String //물음표가 없으면 ClassCastException
+    val result = (a as? String) ?: "실패" //default value
+    println(result)
+}
+
+fun smartCast(a: Any): Int{
+    return if(a is String){
+        a.length
+    }else if(a is Int){
+        a.dec()
+    }else {
+        -1
+    }
+}
