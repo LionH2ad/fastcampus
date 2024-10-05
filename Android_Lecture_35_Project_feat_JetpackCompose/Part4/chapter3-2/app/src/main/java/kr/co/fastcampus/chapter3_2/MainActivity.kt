2148dba48c302166bad093da1fa06def1e6ffc7d
SUNGMIN\sungmin <lion.h2ad@gmail.com>
2024-10-05 23:08:17 +0900

    [FastCampus][Part4][chapter3] Jetpack Compose Text
    [des]
    최초 Project 생성

---
+++ Android_Lecture_35_Project_feat_JetpackCompose/Part4/chapter3-2/app/src/main/java/kr/co/fastcampus/chapter3_2/MainActivity.kt
@@ -0,0 +1,46 @@
+package kr.co.fastcampus.chapter3_2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kr.co.fastcampus.chapter3_2.ui.theme.Chapter32Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Chapter32Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Chapter32Theme {
+        Greeting("Android")
+    }
+}
