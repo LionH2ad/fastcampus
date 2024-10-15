b5b4a2616c8908e1c86542c6a5ea7881eac384b4
SUNGMIN\sungmin <lion.h2ad@gmail.com>
2024-10-16 00:50:17 +0900

    [FastCampus][Part4][chapter6] Jetpack Compose Part4 chapter6 constraintlayout(Card)
    [des]
    Jetpack Compose Part4 chapter6 constraintlayout(Card) 1단계 진행

--- Android_Lecture_35_Project_feat_JetpackCompose/Part4/part4-chapter4-main/part4-chapter4-6/app/src/main/java/kr/co/fastcampus/part4/chapter4_6/MainActivity.kt
+++ Android_Lecture_35_Project_feat_JetpackCompose/Part4/part4-chapter4-main/part4-chapter4-6/app/src/main/java/kr/co/fastcampus/part4/chapter4_6/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import coil.compose.AsyncImage
@@ -54,9 +55,63 @@ fun CardEx(cardData: CardData) {
     ) {
         // 단계 1: 아래의 Row 레이아웃을 ConstraintLayout로 바꾸어 봅시다.
         ConstraintLayout(modifier = Modifier.fillMaxWidth()) {
+            val (profileImage, author, description) = createRefs()
+
+            AsyncImage(
+                model = cardData.imageUri,
+                contentDescription = cardData.imageDescription,
+                contentScale = ContentScale.Crop,
+                placeholder = ColorPainter(color = placeHolderColor),
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(40.dp)
+                    .constrainAs(profileImage) {
+                        centerVerticallyTo(parent)
+                        start.linkTo(parent.start, margin = 8.dp)
+                        /*linkTo(parent.top, parent.bottom)
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)*/
+                    }
+            )
+
+            Text(
+                text = cardData.author,
+                modifier = Modifier.constrainAs(author) {
+                    linkTo(
+                        profileImage.end,
+                        parent.end,
+                        startMargin = 8.dp,
+                        endMargin = 8.dp
+                    )
+                    width = Dimension.fillToConstraints
+                }
+            )
+
+            Text(
+                text = cardData.description,
+                modifier = Modifier.constrainAs(description) {
+                    linkTo(
+                        profileImage.end,
+                        parent.end,
+                        startMargin = 8.dp,
+                        endMargin = 8.dp
+                    )
+                    width = Dimension.fillToConstraints
+                }
+            )
+            val chain = createVerticalChain(
+                author,
+                description,
+                chainStyle = ChainStyle.Packed
+            )
+
+            constrain(chain) {
+                top.linkTo(parent.top, margin = 8.dp)
+                bottom.linkTo(parent.bottom, margin = 8.dp)
+            }
         }
 
-        Row(
+        /*Row(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier.padding(8.dp)
         ) {
@@ -74,7 +129,7 @@ fun CardEx(cardData: CardData) {
                 Text(text = cardData.author)
                 Text(text = cardData.description)
             }
-        }
+        }*/
     }
 }
 
