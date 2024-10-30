a75071d539ca959ba912afc7a41bf94694e32fa1
SUNGMIN\sungmin <lion.h2ad@gmail.com>
2024-10-30 22:51:02 +0900

    [FastCampus][Part5][chapter4] Jetpack Compose CompositionLocal
    [des]
    Jetpack Compose Part5 CompositionLocal 1~6단계 진행

--- Android_Lecture_35_Project_feat_JetpackCompose/Part4/part4-chapter5-main/part4-chapter5-4/app/src/main/java/kr/co/fastcampus/part4/chapter5_4/MainActivity.kt
+++ Android_Lecture_35_Project_feat_JetpackCompose/Part4/part4-chapter5-main/part4-chapter5-4/app/src/main/java/kr/co/fastcampus/part4/chapter5_4/MainActivity.kt
@@ -34,6 +34,7 @@ class MainActivity : ComponentActivity() {
 }
 
 // 단계 4: `compositionLocalOf`에 `8.dp`를 넣어 `LocalElevation`을 할당합니다.
+val LocalElevation = compositionLocalOf{8.dp}
 
 @Composable
 fun Greeting() {
@@ -52,20 +53,34 @@ fun Greeting() {
 
     // 단계 6: LocalElevation의 값을 `CompositionLocalProvider`로
     // 바꾸어 봅시다.
-    Card(
-        modifier = Modifier.padding(8.dp)
-    ) {
-        Column(
-            modifier = Modifier.padding(16.dp),
+    CompositionLocalProvider(LocalElevation provides 12.dp){ // 12.dp 로 오버라이드
+        Card(
+            modifier = Modifier.padding(8.dp),
+            elevation = LocalElevation.current
         ) {
-            Text("안녕하세요. 패스트캠퍼스")
-            Text("스안녕하세요. 패스트캠퍼")
-            Text("퍼스안녕하세요. 패스트캠")
-            Text("캠퍼스안녕하세요. 패스트")
-            Text("트캠퍼스안녕하세요. 패스")
-            Text("스트캠퍼스안녕하세요. 패")
-            Text("패스트캠퍼스안녕하세요.")
-            // 단계 3: `LocalContext.current`의 `resources`를 출력해보세요.
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.disabled){
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                ) {
+                    Text("안녕하세요. 패스트캠퍼스")
+                    Text("${LocalContentColor.current}")
+                    CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.high){
+                        Text("스안녕하세요. 패스트캠퍼")
+                        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium){
+                            Text("퍼스안녕하세요. 패스트캠")
+                        }
+                        CompositionLocalProvider(LocalContentColor provides Color.Magenta){
+                            Text("캠퍼스안녕하세요. 패스트")
+                            Text("트캠퍼스안녕하세요. 패스")
+                            Text("${LocalContentColor.current}")
+                            Text("${LocalContentAlpha.current}")
+                        }
+                    }
+                    Text("스트캠퍼스안녕하세요. 패")
+                    Text("패스트캠퍼스안녕하세요.")
+                    // 단계 3: `LocalContext.current`의 `resources`를 출력해보세요.
+                }
+            }
         }
     }
 }
